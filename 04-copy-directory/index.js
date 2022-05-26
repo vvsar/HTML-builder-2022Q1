5d475b916375110302c14b0ae2ceba78b11df1a4
Victor Saraev <vvsar@mail.ru>
2022-05-26 07:01:21 +0300

    fix: correct solution 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,28 +5,33 @@ const oldDirPath = path.join(__dirname, '/files');
 const newDirPath = path.join(__dirname, '/files-copy');
 
 function copyDir(oldPath, newPath) {
-  fs.mkdir(newPath, { recursive: true }, (error) => {
+  fs.rm(newPath, { recursive: true, force: true }, (error) => {
     if (error) {
       return console.log(error);
     }
-  });
-  fs.readdir(oldPath, { withFileTypes: true }, (error, files) => {
-    if (error) {
-      return console.log(error);
-    }
-    files.forEach((file) => {
-      const oldFilePath = path.join(oldPath, file.name);
-      const newFilePath = path.join(newPath, file.name);
-      if (file.isFile()) {
-        fs.copyFile(oldFilePath, newFilePath, (error) => {
-          if (error) {
-            return console.log(error);
-          }
-        });
+    fs.mkdir(newPath, { recursive: true }, (error) => {
+      if (error) {
+        return console.log(error);
       }
-      if (file.isDirectory()) {
-        copyDir(oldFilePath, newFilePath);
+    });
+    fs.readdir(oldPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        return console.log(error);
       }
+      files.forEach((file) => {
+        const oldFilePath = path.join(oldPath, file.name);
+        const newFilePath = path.join(newPath, file.name);
+        if (file.isFile()) {
+          fs.copyFile(oldFilePath, newFilePath, (error) => {
+            if (error) {
+              return console.log(error);
+            }
+          });
+        }
+        if (file.isDirectory()) {
+          copyDir(oldFilePath, newFilePath);
+        }
+      });
     });
   });
 }
